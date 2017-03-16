776a71f80017815ebe7b513f15872a3fa78510c3
Tony Wu <tony.wuhongtao@gmail.com>
2017-03-16 13:16:08 +0800

    fix some bug

--- client/mobile/js/Player.js
+++ client/mobile/js/Player.js
@@ -63,16 +63,28 @@ var Player = function(startX, startY, color) {
 
 		// Up key takes priority over down
 		if (devOris.up) {
-			y -= 1;
+			if(y > 3+5){
+				y -= 1;
+			}
+			
 		} else if (devOris.down) {
-			y += 1;
+			if(y < canvasHeight-3-5){
+				y += 1;
+			}
+			
 		};
 
 		// Left key takes priority over right
 		if (devOris.left) {
-			x -= 1;
+			if(x > 3+5){
+				x -= 1;
+			}
+			
 		} else if (devOris.right) {
-			x += 1;
+			if(x < canvasWidth-3-5){
+				x += 1;
+			}
+			
 		};
 
 		return (prevX != x || prevY != y) ? true : false;

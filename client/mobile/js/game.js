a9691dd8e39110ddc93fbee4c3ce60d851e4fe2f
Tony Wu <tony.wuhongtao@gmail.com>
2017-03-16 14:10:05 +0800

    change mobile js

--- client/mobile/js/game.js
+++ client/mobile/js/game.js
@@ -8,6 +8,14 @@ var canvas,			// Canvas DOM element
 	localPlayer,	// Local player
 	remotePlayers,	// Remote players
 	socket;			// Socket connection
+var startColor = randomColor();
+
+var ball   = document.querySelector('.ball');
+var garden = document.querySelector('.garden');
+var output = document.querySelector('.output');
+
+var maxX = garden.clientWidth  - ball.clientWidth;
+var maxY = garden.clientHeight - ball.clientHeight;
 
 var serverURL = "http://demo.redline-china.com";
 // var serverURL = "http://localhost";
@@ -15,6 +23,11 @@ var serverURL = "http://demo.redline-china.com";
 ** GAME INITIALISATION
 **************************************************/
 function init() {
+
+
+
+
+
 	// Declare the canvas and rendering context
 
 	// Maximise the canvas
@@ -31,7 +44,7 @@ function init() {
 	// placed right on the egde of the screen
 	var startX = Math.round(Math.random()*(canvasWidth-3-5)),
 		startY = Math.round(Math.random()*(canvasHeight-3-5));
-	var startColor = randomColor();
+	
 
 	// Initialise the local player
 
@@ -45,8 +58,33 @@ function init() {
 
 	// Start listening for events
 	setEventHandlers();
+
 };
 
+function handleOrientation(event) {
+	var x = event.beta;  // In degree in the range [-180,180]
+	var y = event.gamma; // In degree in the range [-90,90]
+
+	output.innerHTML  = "beta : " + x + "\n";
+	output.innerHTML += "gamma: " + y + "\n";
+
+	// Because we don't want to have the device upside down
+	// We constrain the x value to the range [-90,90]
+	if (x >  90) { x =  90};
+	if (x < -90) { x = -90};
+
+	// To make computation easier we shift the range of 
+	// x and y to [0,180]
+	x += 90;
+	y += 90;
+
+	// 10 is half the size of the ball
+	// It center the positioning point to the center of the ball
+	ball.style.top  = (maxX*x/400 - 10) + "px";
+	ball.style.left = (maxY*y/400 - 10) + "px";
+	ball.style.background = startColor;
+	
+}
 
 /**************************************************
 ** GAME EVENT HANDLERS
@@ -69,6 +107,7 @@ function onMove(e) {
 	if(localPlayer) {
 		devOris.onMove(e);
 	}
+	handleOrientation(e);
 }
 
 // Socket connected
